25221ed9e50c616cbed275e7907338c1ae3778a4
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-03-28 17:15:52 -0300

    inserção dados veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -29,11 +29,11 @@ class Cor(models.Model):
         return self.descricao
     
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca ) #
-    categoria = models.ForeignKey(Categoria)
-    cor = models.ForeignKey(Cor)
-    ano = models.IntegerField(max_digits=4, default=0, null=True, blank=True)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     
     def __str__(self):
-        return f"{self.marca}, {self.modelo}, {self.ano}, {self.cor} "
+        return f"{self.marca}, {self.modelo}, ({self.ano}), ({self.cor})"
