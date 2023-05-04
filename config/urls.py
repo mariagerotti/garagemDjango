faafcd6d7e0f279036a7b1cce6b6ef71e9c0f3a6
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-05-04 13:26:55 -0300

    criação API classe Categoria

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet
+from garagem.views import CategoriaViewSet, MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"Marca", MarcaViewSet)
+router.register(r"Categoria", CategoriaViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- garagem/views.py
+++ garagem/views.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
 from garagem.models import Marca
-from garagem.serializers import MarcaSerializer
+from garagem.serializers import CategoriaSerializer, MarcaSerializer
 
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
