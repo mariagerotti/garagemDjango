68fd2364fe505f9d9116b3bd32edc556a589985a
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-08-12 14:52:12 -0300

    change relation in tables

--- garagem/models.py
+++ garagem/models.py
@@ -36,16 +36,31 @@ class Cor(models.Model):
         verbose_name_plural = "Cores"
 
 
-class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+class Modelo(models.Model):
+    nome = models.CharField(max_length=50)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="modelos")
     categoria = models.ForeignKey(
-        Categoria, on_delete=models.PROTECT, related_name="veiculos"
+        Categoria, on_delete=models.PROTECT, related_name="modelos"
     )
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Modelo"
+        verbose_name_plural = "Modelos"
+
+
+class Veiculo(models.Model):
     ano = models.IntegerField(default=0, null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(
+        Modelo, on_delete=models.PROTECT, related_name="veiculos"
+    )
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=True, blank=True
     )
+    descricao = models.CharField(max_length=100, default="descricao", null=True, blank=True)
 
     def __str__(self):
         return f"{self.marca}, ({self.ano}), ({self.cor})"
