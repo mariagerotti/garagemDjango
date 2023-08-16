6fb7ac300cd0bf8ba320522adbd4eed9e0685cee
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-08-16 18:35:11 -0300

    organização pastas views

--- garagem/views.py
+++ garagem/views.py
@@ -1,6 +1,4 @@
-from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Acessorio, Categoria, Cor, Modelo, Marca, Veiculo
 from garagem.serializers import (
     AcessorioSerializer,
     CategoriaSerializer,
@@ -11,38 +9,3 @@ from garagem.serializers import (
     VeiculoListSerializer,
     ModeloSerializer
 )
-
-
-class AcessorioViewSet(ModelViewSet):
-    queryset = Acessorio.objects.all()
-    serializer_class = AcessorioSerializer
-    
-class ModeloViewSet(ModelViewSet):
-    queryset = Modelo.objects.all()
-    serializer_class = ModeloSerializer
-
-
-class CategoriaViewSet(ModelViewSet):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
-
-
-class CorViewSet(ModelViewSet):
-    queryset = Cor.objects.all()
-    serializer_class = CorSerializer
-
-
-class MarcaViewSet(ModelViewSet):
-    queryset = Marca.objects.all()
-    serializer_class = MarcaSerializer
-
-
-class VeiculoViewSet(ModelViewSet):
-    queryset = Veiculo.objects.all()
-    serializer_classes = {
-        "list": VeiculoListSerializer,
-        "retrieve": VeiculoDetailSerializer,
-    }
-
-    def get_serializer_class(self):
-        return self.serializer_classes.get(self.action, VeiculoSerializer)
---
+++ garagem/views/VeiculoViewSet.py
@@ -0,0 +1,18 @@
+from rest_framework.viewsets import ModelViewSet
+
+from garagem.models import Veiculo
+from garagem.serializers import (
+    VeiculoSerializer,
+    VeiculoListSerializer,
+    VeiculoDetailSerializer,
+)
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_classes = {
+        "list": VeiculoListSerializer,
+        "retrieve": VeiculoDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, VeiculoSerializer)
